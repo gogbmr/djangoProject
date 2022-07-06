3550a2f008f45403b51631728d8b248ff969045b
Deepakvan <deepakbmr07@gmail.com>
2022-07-06 13:26:32 +0530

    second commit

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.index,name="home page")
-
-
+    path('', views.index,name="home page"),
+    path('courses',views.Courses,name="Courses"),
+    path('<int:course_id>/', views.details,name="details")
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-
+from .models import AllCourses, Details
 # Create your views here.
-from django.http import HttpResponse
-
+from django.http import HttpResponse,Http404
+from django.template import loader
 def index(request):
     return HttpResponse('Hello, World! by deepak')
+
+def Courses(request):
+    ac=AllCourses.objects.all()
+    template=loader.get_template('my_app/courses.html')
+    context={'ac':ac,}
+    return HttpResponse(template.render(context,request))
+
+def details(request,course_id):
+    try:
+        course=AllCourses.objects.get(pk=course_id)
+    except AllCourses.DoesNotExist:
+        raise Http404("Course not available")
+    return render(request,'my_app/details.html',{'coursename':course.coursename})
